418393737b038910ca97a37572ffe756dcfa415a
kayiwadaniel <kayiwadaniel@3635ef14-fccd-11dd-a320-db7d8e7a638f>
2015-06-14 14:31:12 +0000

    Encoding url when saving query

--- branches/1.0.1/QueryBuilder/src/org/purc/purcforms/client/querybuilder/view/SaveQueryDialog.java
+++ branches/1.0.1/QueryBuilder/src/org/purc/purcforms/client/querybuilder/view/SaveQueryDialog.java
@@ -8,6 +8,7 @@ import org.purc.purcforms.client.util.FormUtil;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.Button;
@@ -82,7 +83,7 @@ public class SaveQueryDialog extends DialogBox{
 						action += "&";
 					else
 						action += "?";
-					action += "queryName=" + queryName;
+					action += "queryName=" + URL.encodeQueryString(queryName);
 					
 					form.setAction(action);
 					((VerticalPanel)txtName.getParent()).add(txtArea);
